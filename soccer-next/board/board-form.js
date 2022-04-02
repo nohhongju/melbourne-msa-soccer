04fd7fe4a5e6fac9b587ec22ae687a5ad788748d
nohhongju <ghdwn49@gmail.com>
2022-04-02 23:46:14 +0900

    2022-04-02 team-form 데이터셋만들기

--- soccer-next/board/board-form.js
+++ soccer-next/board/board-form.js
@@ -21,37 +21,38 @@ export default function BoardhtmlForm(){
             <htmlForm action="">
             <div className={style.row}>
                 <div className={style.col25}>
-                <label className={style.label} htmlFor="passengerId">PassengerId</label>
+                <label className={style.label} htmlFor="passengerId">게시글 작성자 ID</label>
                 </div>
                 <div className={style.col75}>
                 <input type="text" className={style.inputText} id="passengerId" 
-                name="passengerId" placeholder="사용자 id 입력" onChange={handleChange}/>
+                name="passengerId" placeholder="게시글 작성자 ID 입력" onChange={handleChange}/>
                 </div>
             </div>
             <div className={style.row}>
                 <div className={style.col25}>
-                <label htmlFor="name">Name</label>
+                <label htmlFor="name">게시글 작성자 이름</label>
                 </div>
                 <div className={style.col75}>
                 <input type="text" id="name" name="name" 
-                placeholder="사용자 이름 입력" onChange={handleChange}/>
+                placeholder="게시글 작성자 이름 입력" onChange={handleChange}/>
                 </div>
             </div>
             <div className={style.row}>
                 <div className={style.col25}>
-                <label htmlFor="team">Team</label>
+                <label htmlFor="team">응원팀</label>
                 </div>
                 <div className={style.col75}>
                 <select id="teamId" name="teamId" onChange={handleChange}>
-                    <option value="K09" selected>서울 FC서울</option>
-                    <option value="K02">수원 삼성블루윙즈</option>
-                    <option value="K04">인천 유나이티드</option>
+                    <option value="">응원팀 선택</option>
+                    <option value="K09">Fc seoul</option>
+                    <option value="K02">Suwon Samseong blue wings</option>
+                    <option value="K04">Incheon United</option>
                 </select>
                 </div>
             </div>
             <div className={style.row}>
                 <div className={style.col25}>
-                <label htmlFor="subject">Subject</label>
+                <label htmlFor="subject">게시글 내용</label>
                 </div>
                 <div className={style.col75}>
                 <textarea id="subject" name="subject" placeholder="게시글 입력" 
--- soccer-next/game/team-form.js
+++ soccer-next/game/team-form.js
@@ -3,9 +3,17 @@ import { useState } from "react"
 
 export default function TeamForm(){
     const [inputs, setInputs] = useState({})
+    const {TeamId,orgYyyy,stadiumName,address,tel} = inputs;
+
+    const handleChange = e => {
+        e.preventDefault()
+        const {name, value} = e.target;
+        setInputs({...inputs, [name]:value})
+    }
     const handleSubmit = e => {
         e.preventDefault()
-        alert(`등록할 팀 정보 : `)
+        const Request = {TeamId,orgYyyy,stadiumName,address,tel}
+        alert(`등록할 팀 정보 : ${JSON.stringify(Request)}`)
     }
     return (<>
         <div className={style.container}>
@@ -15,7 +23,7 @@ export default function TeamForm(){
                     <label className={style.label} htmlFor="teamId">팀 ID</label>
                 </div>
                 <div className={style.col75}>
-                    <input type="text" className={style.inputText} id="TeamId" name="TeamId"/>
+                    <input type="text" className={style.inputText} id="TeamId" name="TeamId" onChange={handleChange}/>
                 </div>
             </div>
             <div className={style.row}>
@@ -23,7 +31,7 @@ export default function TeamForm(){
                     <label className={style.label} htmlFor="teamId">팀 ID</label>
                 </div>
                 <div className={style.col75}>
-                    <input type="text" className={style.inputText} id="TeamId" name="TeamId"/>
+                    <input type="text" className={style.inputText} id="TeamId" name="TeamId" onChange={handleChange}/>
                 </div>
             </div>
             <div className={style.row}>
@@ -31,7 +39,7 @@ export default function TeamForm(){
                     <label className={style.label} htmlFor="teamId">연고지</label>
                 </div>
                 <div className={style.col75}>
-                    <input type="text" className={style.inputText} id="TeamId" name="TeamId"/>
+                    <input type="text" className={style.inputText} id="TeamId" name="TeamId" onChange={handleChange}/>
                 </div>
             </div>
             <div className={style.row}>
@@ -39,7 +47,7 @@ export default function TeamForm(){
                     <label className={style.label} htmlFor="teamId">팀명</label>
                 </div>
                 <div className={style.col75}>
-                    <input type="text" className={style.inputText} id="TeamId" name="TeamId"/>
+                    <input type="text" className={style.inputText} id="TeamId" name="TeamId" onChange={handleChange}/>
                 </div>
             </div>
             <div className={style.row}>
@@ -47,7 +55,7 @@ export default function TeamForm(){
                     <label className={style.label} htmlFor="orgYyyy">창단년도</label>
                 </div>
                 <div className={style.col75}>
-                    <input type="text" className={style.inputText} id="orgYyyy" name="orgYyyy"/>
+                    <input type="text" className={style.inputText} id="orgYyyy" name="orgYyyy" onChange={handleChange}/>
                 </div>
             </div>
             <div className={style.row}>
@@ -55,7 +63,7 @@ export default function TeamForm(){
                     <label className={style.label} htmlFor="stadiumName">스타디움 명칭</label>
                 </div>
                 <div className={style.col75}>
-                    <input type="text" className={style.inputText} id="stadiumName" name="stadiumName"/>
+                    <input type="text" className={style.inputText} id="stadiumName" name="stadiumName" onChange={handleChange}/>
                 </div>
             </div>
             <div className={style.row}>
@@ -63,7 +71,7 @@ export default function TeamForm(){
                     <label className={style.label} htmlFor="address">주소</label>
                 </div>
                 <div className={style.col75}>
-                    <input type="text" className={style.inputText} id="address" name="address"/>
+                    <input type="text" className={style.inputText} id="address" name="address" onChange={handleChange}/>
                 </div>
             </div>
             <div className={style.row}>
@@ -71,13 +79,13 @@ export default function TeamForm(){
                     <label className={style.label} htmlFor="tel">전화번호</label>
                 </div>
                 <div className={style.col75}>
-                    <input type="text" className={style.inputText} id="tel" name="tel"/>
+                    <input type="text" className={style.inputText} id="tel" name="tel" onChange={handleChange}/>
                 </div>
             </div>
             <br/>
             <div className={style.row}>
                 <input type="submit" className={style.inputSubmit}
-                value="Submit"/>
+                value="Submit"onClick={handleSubmit}/>
             </div>
             </form>
             </div>
